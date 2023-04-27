21317a773b59b9277e3a54bc05483c6f3191a007
Chewycide <gitchewycide@gmail.com>
2023-04-27 21:13:27 +0800

    Refactor Code

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,5 +1,10 @@
 import gui.MainWindow;
 
+/**
+ * The Main Launcher of the Application.
+ * The Launcher is required to deal with the module system trouble
+ * that JavaFX and Maven is making.
+ */
 public class Launcher {
     public static void main(String[] args) {
         MainWindow.main(args);
