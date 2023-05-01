2be834a4c1dc641c0c3c0498cddacb26a3181e82
Chewycide <gitchewycide@gmail.com>
2023-05-01 10:39:15 +0800

    Implement LoginWindow class
    The LoginWindow is an inner class that is used to prompt the user for a
    host and port that the server will be bound to. After the user enters
    the requirements, then the main window will be shown.

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -8,8 +8,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import java.io.IOException;
+import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
 
 
 /**
@@ -18,23 +18,20 @@ import java.io.IOException;
  */
 public class MainWindow extends Application {
     private ServerManager server_manager;
-
-
-    /**
-     * Create a server during the initialization phase of the server.
-     *
-     * @throws IOException error when opening a socket
-     */
-    @Override
-    public void init() throws IOException {
-        server_manager = new ServerManager("0.0.0.0", 62609);
-        new Thread(server_manager).start();
-    }
+    private LoginWindow login_window;
+    private Stage primary_stage;
 
 
     @Override
-    public void start(Stage primary_stage) {
+    public void start(Stage stage) {
+        login_window = new LoginWindow(new Stage());
+        login_window.initUI();
+        login_window.showAndWait();
+
+        primary_stage = stage;
+        initUI();
         primary_stage.show();
+
     }
 
 
@@ -44,9 +41,110 @@ public class MainWindow extends Application {
      */
     @Override
     public void stop() {
-        if (!server_manager.isClosed()) {
-            // TODO: remind the user to close the server first before closing.
-            server_manager.stopServer();
+        // TODO: remind the user to close the server first before closing.
+    }
+
+
+    private void initUI() {
+    }
+
+
+    /**
+     * The LoginWindow is a nested class that will appear during the initialization
+     * phase of the program. it will ask the user what host and port should the
+     * server be bound to.
+     */
+    private class LoginWindow {
+        private int port;
+        private String host;
+        private final Stage login_stage;
+
+
+        /**
+         * Instantiate a LoginWindow object.
+         *
+         * @param stage the stage object. should be undecorated.
+         */
+        LoginWindow (Stage stage) {
+            this.login_stage = stage;
+            this.login_stage.setResizable(false);
+        }
+
+
+        /**
+         * Initialize the UI of the window.
+         */
+        public void initUI() {
+            // ----- Layout ----- //
+            VBox root = new VBox();
+
+            // ----- Row 1 ----- //
+            HBox row1 = new HBox();
+            root.getChildren().add(row1);
+            Label host_label = new Label("Bind to host:");
+            TextField host_textfield = new TextField("0.0.0.0");
+            row1.getChildren().addAll(host_label, host_textfield);
+
+            // ----- Row 2 ----- //
+            HBox row2 = new HBox();
+            root.getChildren().add(row2);
+            Label port_label = new Label("Bind to port: ");
+            TextField port_textfield = new TextField("62609");
+            row2.getChildren().addAll(port_label, port_textfield);
+
+            // ----- Scene ----- //
+            HBox row3 = new HBox();
+            row3.setAlignment(Pos.CENTER);
+            root.getChildren().add(row3);
+            Button enter_button = new Button("Enter");
+            row3.getChildren().addAll(enter_button);
+            enter_button.setOnAction(e -> {
+                // Assign the text inputs from the text fields to the respective variables.
+                host = host_textfield.getText();
+                port = Integer.parseInt(port_textfield.getText());
+                closeWindow();
+            });
+
+
+            // ----- Scene ----- //
+            Scene scene = new Scene(root); // set the main layout of the scene.
+            login_stage.setScene(scene); // set the main scene.
+        }
+
+
+        /**
+         * Get the host input from the text field.
+         *
+         * @return the host to be bound to.
+         */
+        public String getHost() {
+            return host;
+        }
+
+
+        /**
+         * Get the port input from the text field.
+         *
+         * @return the port to be bound to
+         */
+        public int getPort() {
+            return port;
+        }
+
+
+        /**
+         * Show the window/stage while blocking the current thread.
+         */
+        public void showAndWait() {
+            login_stage.showAndWait();
+        }
+
+
+        /**
+         * Closes the window/stage.
+         */
+        public void closeWindow() {
+            login_stage.close();
         }
     }
 
