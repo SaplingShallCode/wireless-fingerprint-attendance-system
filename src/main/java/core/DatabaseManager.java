4312dcc578d1d9f52990b0358db3da1977987812
Chewycide <gitchewycide@gmail.com>
2023-06-11 16:15:14 +0800

    Add Enroll to Database Function And Properly Close DB Connections

--- src/main/java/core/DatabaseManager.java
+++ src/main/java/core/DatabaseManager.java
@@ -1,10 +1,8 @@
 package core;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import utility.TempEnrollmentData;
+import java.sql.*;
 
 /**
  * The DatabaseManager class will handle the database communication and
@@ -23,7 +21,10 @@ public class DatabaseManager {
 
     /**
      * Open a connection to the database.
-     * This method is to be used before executing SQL commands.
+     * <p>
+     *     NOTE: The connection object should be closed using the method
+     *     {@link #closeThis(Connection)}.
+     * </p>
      * @return a connection object
      * @throws SQLException if a database access error or the url is null.
      */
@@ -43,15 +44,65 @@ public class DatabaseManager {
         return connection;
     }
 
+    /**
+     * Closes a non-null connection.
+     * @param connection Closable connection
+     */
+    public void closeThis(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            }
+            catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * Overloaded method of {@link #closeThis(Connection)}.
+     * @param resultSet Closable result set
+     */
+    public void closeThis(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            }
+            catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        }
+    }
+
+
+    /**
+     * Overloaded method of {@link #closeThis(Connection)}.
+     * @param statement Closable statement
+     */
+    public void closeThis(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            }
+            catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        }
+    }
+
 
     /**
      * Create the necessary tables. If the table already exists then the
      * SQL command will not execute as stated in the script "IF NOT EXISTS".
      */
-    private void initTables() {
+    public boolean initTables() {
+        Connection connection = null;
+        Statement stmt = null;
+        boolean isSuccessful = true;
         try {
-            Connection connection = openConnection();
-            Statement stmt = connection.createStatement();
+            connection = openConnection();
+            stmt = connection.createStatement();
 
             String create_users_t = "CREATE TABLE IF NOT EXISTS users (" +
                     "user_id serial NOT NULL PRIMARY KEY, " +
@@ -96,14 +147,96 @@ public class DatabaseManager {
 
             int[] create_table_feedback = stmt.executeBatch();
             for (int feedback : create_table_feedback) {
-                System.out.println((feedback != Statement.EXECUTE_FAILED) ? "Init Table OK" : "Init Table NOT_OK");
+                isSuccessful = feedback != Statement.EXECUTE_FAILED;
             }
-
-            stmt.close();
-            connection.close();
         }
         catch (SQLException sqle) {
             sqle.printStackTrace();
+            isSuccessful = false;
         }
+        finally {
+            closeThis(stmt);
+            closeThis(connection);
+        }
+        return isSuccessful;
+    }
+
+
+    /**
+     * @param enrollee_data A temporary data object: {@link TempEnrollmentData}
+     * @return true if enrollment record is successfully created within the database,
+     * otherwise false.
+     */
+    public boolean enrollUser(TempEnrollmentData enrollee_data) {
+        boolean isSuccessful = true;
+        Connection connection = null;
+        PreparedStatement users_stmt = null;
+        PreparedStatement get_user_id_stmt = null;
+        ResultSet result = null;
+        PreparedStatement user_info_stmt = null;
+        try {
+            connection = openConnection();
+
+            // Inserting record to the users table.
+            String users_record = "INSERT INTO users (" +
+                    "full_name, " +
+                    "fingerprint_id) " +
+                    "VALUES (?, ?)";
+            users_stmt = connection.prepareStatement(users_record);
+            users_stmt.setString(1, enrollee_data.getFullName());
+            users_stmt.setInt(2, enrollee_data.getFingerprintId());
+
+            users_stmt.executeUpdate();
+
+
+            // get the user_id of the recently added user in the users table.
+            String get_user_id = "SELECT user_id FROM users " +
+                    "WHERE full_name = ? " +
+                    "AND fingerprint_id = ?";
+            get_user_id_stmt = connection.prepareStatement(get_user_id);
+            get_user_id_stmt.setString(1, enrollee_data.getFullName());
+            get_user_id_stmt.setInt(2, enrollee_data.getFingerprintId());
+            result = get_user_id_stmt.executeQuery();
+            int user_id = 0;
+            if (result.next()) {
+                user_id = result.getInt("user_id");
+            }
+
+
+            // Inserting record to the user_info table.
+            String user_info_record = "INSERT INTO user_info (" +
+                    "user_id, " +
+                    "first_name, " +
+                    "middle_name, " +
+                    "last_name, " +
+                    "age, " +
+                    "gender, " +
+                    "phone_number, " +
+                    "address) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            user_info_stmt = connection.prepareStatement(user_info_record);
+            user_info_stmt.setInt(1, user_id);
+            user_info_stmt.setString(2, enrollee_data.getFirstName());
+            user_info_stmt.setString(3, enrollee_data.getMiddleName());
+            user_info_stmt.setString(4, enrollee_data.getLastName());
+            user_info_stmt.setShort(5,  enrollee_data.getAge());
+            user_info_stmt.setString(6, enrollee_data.getGender());
+            user_info_stmt.setString(7, enrollee_data.getPhoneNumber());
+            user_info_stmt.setString(8, enrollee_data.getAddress());
+
+            user_info_stmt.executeUpdate();
+        }
+        catch (SQLException sqle) {
+            sqle.printStackTrace();
+            isSuccessful = false;
+        }
+        finally {
+            closeThis(users_stmt);
+            closeThis(get_user_id_stmt);
+            closeThis(result);
+            closeThis(user_info_stmt);
+            closeThis(connection);
+        }
+        return isSuccessful;
     }
 }
